30b255975aeca415b759d7ae2e2e514d35ef4aa9
Koji Yoshida <y1413191@gmail.com>
2021-12-25 02:22:21 +0900

    first commit

--- app/validators/attachment_validator.rb
+++ app/validators/attachment_validator.rb
@@ -1,5 +1,5 @@
 class AttachmentValidator < ActiveModel::EachValidator
-  include ActiveSupport::NumberHelper
+  include ActiveSupport::NumberHelper # ①
 
   def validate_each(record, attribute, value)
     return if value.blank? || !value.attached?
@@ -7,11 +7,18 @@ class AttachmentValidator < ActiveModel::EachValidator
     has_error = false
 
     if options[:maximum]
-      has_error = true unless validate_maximum(record, attribute, value)
-    end
-
-    if options[:content_type]
-      has_error = true unless validate_content_type(record, attribute, value)
+      if value.is_a?(ActiveStorage::Attached::Many) #③
+        # 画像が複数枚投稿された場合
+        value.each do |v|
+          unless validate_maximum(record, attribute, v)
+            has_error = true
+            break
+          end
+        end
+      else
+        # 画像が1枚投稿された場合
+        has_error = true unless validate_maximum(record, attribute, value)
+      end
     end
 
     record.send(attribute).purge if options[:purge] && has_error
@@ -20,7 +27,7 @@ class AttachmentValidator < ActiveModel::EachValidator
   private
 
   def validate_maximum(record, attribute, value)
-    if value.byte_size > options[:maximum]
+    if value.byte_size > options[:maximum]#②
       record.errors[attribute] << (options[:message] || "は#{number_to_human_size(options[:maximum])}以下にしてください")
       false
     else
@@ -28,12 +35,4 @@ class AttachmentValidator < ActiveModel::EachValidator
     end
   end
 
-  def validate_content_type(record, attribute, value)
-    if value.content_type.match?(options[:content_type])
-      true
-    else
-      record.errors[attribute] << (options[:message] || 'は対応できないファイル形式です')
-      false
-    end
-  end
 end
